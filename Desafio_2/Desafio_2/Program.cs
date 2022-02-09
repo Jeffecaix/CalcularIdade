281bdae4106fa6b48c4725dba85c2f67c1f389f2
Jefferson Cais <73970429+Jeffecaix@users.noreply.github.com>
2022-02-09 19:12:16 -0300

    Add files via upload

---
+++ Desafio_2/Desafio_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Desafio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data1 = "Vinicius 06/12/89";
+
+            string[] nomeData = data1.Split(" ");
+            string nome = nomeData[0];
+
+            DateTime dataCompleta = DateTime.Parse(nomeData[1]);
+            DateTime dataAtual = DateTime.Parse("10/02/22");
+            
+            string CalculaIdadeDias(DateTime dataNascimento, DateTime dataAtual)
+            {
+                TimeSpan resultado = dataAtual - dataNascimento;
+                return $"{nome} viveu até hoje {resultado.Days} dias.";
+            }
+
+            Console.WriteLine(CalculaIdadeDias(dataCompleta, dataAtual));           
+            
+        }
+    }
+}
